1d98857c7e6b850a08aa62fda60a53c3b2c03067
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-08-16 18:11:32 +0800

    Add files via upload

--- baiyibutie15-8.js
+++ baiyibutie15-8.js
@@ -94,8 +94,8 @@ function exchange() {
           console.log(data);
           if (safeGet(data)) {
             data = JSON.parse(data);;
-            console.log(`【百亿补贴15-8优惠券抢券结果】${JSON.stringify(data)}\n`);
-			notify.sendNotify(`\n【京东账号${$.index}】${$.nickName}\n【百亿补贴15-8优惠券抢券结果】${$.data}`);
+            console.log(`【百亿补贴15减8优惠券抢券结果】${JSON.stringify(data)}\n`);
+			notify.sendNotify(`\n【京东账号${$.index}】${$.nickName}\n【百亿补贴15减8优惠券抢券结果】${$.data}`);
           }
         }
       } catch (e) {
