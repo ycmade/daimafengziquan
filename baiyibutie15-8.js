8f8d22ef8087b116dd7b8f4dcd998b569b24af01
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-08-19 21:55:07 +0800

    Update baiyibutie15-8.js

--- baiyibutie15-8.js
+++ baiyibutie15-8.js
@@ -95,7 +95,7 @@ function exchange() {
           if (safeGet(data)) {
             data = JSON.parse(data);;
             console.log(`【百亿补贴15减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}\n`);
-			notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【百亿补贴15减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}`);
+	//notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【百亿补贴15减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}`);
           }
         }
       } catch (e) {
