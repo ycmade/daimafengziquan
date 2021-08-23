872a4ccb1851c9d7c8a805d7bd2bc38fd112eeb2
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-08-23 12:16:11 +0800

    Update butiejie9.9-8.js

--- butiejie9.9-8.js
+++ butiejie9.9-8.js
@@ -96,7 +96,7 @@ function exchange() {
             data = JSON.parse(data);
             console.log(`【补贴节9.9减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}\n`);
 						//自行添加通知项目开始
-			if(${JSON.stringify(data.subCodeMsg)}.indexOf('领取成功') !=-1){
+			if(${data.subCodeMsg.indexOf('领取成功') !=-1){
 				notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【补贴节9.9减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}`);
 				}
 			//自行添加通知项目结束
