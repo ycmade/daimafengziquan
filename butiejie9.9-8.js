b6129d3b7243af003a15d7412b5acb6ca09d10e1
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-08-23 12:22:04 +0800

    Add files via upload

--- butiejie9.9-8.js
+++ butiejie9.9-8.js
@@ -95,8 +95,9 @@ function exchange() {
           if (safeGet(data)) {
             data = JSON.parse(data);
             console.log(`【补贴节9.9减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}\n`);
-						//自行添加通知项目开始
-			if(${data.subCodeMsg.indexOf('领取成功') !=-1){
+			//自行添加通知项目开始
+			var str="{"+data.subCodeMsg+"}";
+			if(${str.indexOf('领取成功') !=-1){
 				notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【补贴节9.9减8优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}`);
 				}
 			//自行添加通知项目结束
